40ff23abe7bf5f9366ce2a8d07abd4d466ce09f1
felipy <felipy81br@gmail.com>
2024-03-26 10:12:13 -0300

    9-streaming react component, suspense, skeletons, wrapper skeleton

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -37,6 +37,9 @@ export async function fetchRevenue() {
 export async function fetchLatestInvoices() {
   noStore();
   try {
+    console.log('Fetching latest invoices...');
+    await new Promise((resolve) => setTimeout(resolve, 4000));
+
     const data = await sql<LatestInvoiceRaw>`
       SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
       FROM invoices
